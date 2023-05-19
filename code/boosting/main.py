b2fe1d0cc3279f2f9cee77cd8539eef9776a18cd
Kim Jiwoo <rlawldn11703@gmail.com>
2023-05-19 19:52:10 +0900

    [Feat] Modify main.py
     #4
    Signed-off-by: Kim Jiwoo <rlawldn11703@gmail.com>

--- code/boosting/main.py
+++ code/boosting/main.py
@@ -4,19 +4,20 @@ import numpy as np
 import pandas as pd
 
 from boosting.args import parse_args
-from boosting.dataloader import Dataset, Preprocess
-from boosting.utils import get_logger, set_seeds, logging_conf
-from boosting.model import boosting_model
+from boosting.data_loader import Dataset, Preprocess
+from boosting.utils import set_seeds
+from boosting.boosting import boosting_model
 
+import warnings
 
-logger = get_logger(logging_conf)
+warnings.filterwarnings("ignore")
 
 
 def main(args):
     ######################## DATA LOAD
     print("Load Data")
-    train = pd.read_csv(args.data_dir + args.file_name)
-    test = pd.read_csv(args.data_dir + args.test_file_name)
+    train = pd.read_csv(args.data_dir + args.file_name, parse_dates=["Timestamp"])
+    test = pd.read_csv(args.data_dir + args.test_file_name, parse_dates=["Timestamp"])
 
     data = Dataset(train, test)
     data = data.split_data()
@@ -24,20 +25,21 @@ def main(args):
 
     ######################## SELECT FEATURE
 
-    FEATURE = []
+    FEATURE = ["userID", "assessmentItemID", "Timestamp", "KnowledgeTag"]
 
     ######################## DATA PREPROCESSING
 
-    data = Preprocess(data, FEATURE)
+    print("Start Preprocessing Data")
+    process = Preprocess(args, data, FEATURE)
+    data = process.preprocess()
+    print("Succesfully Preprocess Data")
 
     ######################## MODEL INIT
 
     model = boosting_model(args, FEATURE)
 
-    ######################## TRAIN
-
-    model.training(data)
-
+    #     ######################## TRAIN
+    model.training(data, args)
     model.inference(data)
 
 
