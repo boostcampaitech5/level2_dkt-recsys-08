36808539f4dfea43c42bc45b079b97cd48bff1ef
Kim Jiwoo <rlawldn11703@gmail.com>
2023-05-22 11:52:36 +0900

    [Feat] Add new feature & code to check output_dir
     #4
    
    Signed-off-by: Kim Jiwoo <rlawldn11703@gmail.com>

--- code/boosting/main.py
+++ code/boosting/main.py
@@ -25,12 +25,37 @@ def main(args):
 
     ######################## SELECT FEATURE
 
-    FEATURE = ["userID", "assessmentItemID", "Timestamp", "KnowledgeTag"]
+    FEATURE = [
+        "userID",
+        "assessmentItemID",
+        "KnowledgeTag",
+        "elapsed",
+        "category_high",
+        "timeDelta_userAverage",
+        "timestep_1",
+        "timestep_2",
+        "timestep_3",
+        "timestep_4",
+        "timestep_5",
+    ]
+    FEATURE_USER = ["user_answer_mean", "user_answer_cnt", "user_time_mean"]
+    FEATURE_ITEM = [
+        "item_answer_mean",
+        "item_answer_cnt",
+        "item_time_mean",
+        "wrong_users_mean_elapsed",
+        "correct_users_mean_elapsed",
+    ]
+    FEATURE_TAG = ["tag_exposed", "tag_answer_rate"]
+
+    FEATURE += FEATURE_USER
+    FEATURE += FEATURE_ITEM
+    FEATURE += FEATURE_TAG
 
     ######################## DATA PREPROCESSING
 
     print("Start Preprocessing Data")
-    process = Preprocess(args, data, FEATURE)
+    process = Preprocess(args, data)
     data = process.preprocess()
     print("Succesfully Preprocess Data")
 
@@ -38,8 +63,14 @@ def main(args):
 
     model = boosting_model(args, FEATURE)
 
-    #     ######################## TRAIN
+    ######################## TRAIN
+
     model.training(data, args)
+
+    ######################## INFERENCE
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir)
     model.inference(data)
 
 
