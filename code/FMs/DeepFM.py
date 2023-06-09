f7c6dab2b329cdb25d543f314f188acd526c26de
jeujeon <1132jjw@naver.com>
2023-06-09 16:15:23 +0900

    [Feat] Add DeepFM model and dataset

---
+++ code/FMs/DeepFM.py
@@ -0,0 +1,11 @@
+import torch
+from abc import *
+
+
+# book rating baseline deepFM 코드 활용
+class DEEP_FM_Model:
+    def __init__(self, data, embed_dim=16, mlp_dims=(16, 16), dropout=0.2):
+        pass
+
+    def forward(self, x: torch.Tensor):
+        pass
